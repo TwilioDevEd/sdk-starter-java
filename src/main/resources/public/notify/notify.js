8bb98f9977ba1d396ae3a87d7716addacf9c0292
idelgado <idelgado@twilio.com>
2017-06-20 14:58:39 -0400

    Add "Hello, World!"

--- src/main/resources/public/notify/notify.js
+++ src/main/resources/public/notify/notify.js
@@ -1,7 +1,7 @@
 $(function() {
     $('#sendNotificationButton').on('click', function() {
         var identity = $('#identityInput').val();
-        var data = JSON.stringify({ "identity" : [ identity ]});
+        var data = JSON.stringify({ "identity" : [ identity ], "body" : "Hello, World!" });
         $.post('/send-notification', data, function(response) {
             $('#identityInput').val('');
             $('#message').html(response.message);
